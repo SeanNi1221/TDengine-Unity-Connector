aa89f64ee90009bf921f34f6bf7dbc0b988f8cda
SeanNi1221 <sean.ni.21@outlook.com>
2021-07-29 09:45:11 +0800

    Primitive features finished
    1.Authorization: Basic;
    2.API: TDBridge.SQL.

---
+++ TDBridge/Assets/Bridge/Editor/TDBridgeEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Sean.Bridge;
+
+[CustomEditor(typeof(TDBridge))]
+public class TDBridgeEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        TDBridge td = (TDBridge)target;
+        if (GUILayout.Button("Initialize TDBridge"))
+        {
+            td.Initialize();
+        }
+        DrawDefaultInspector();
+        if (GUILayout.Button("Execute SQL", GUILayout.Height(32)))
+        {
+            TDBridge.SQL(td.sql);
+        }
+    }
+}
