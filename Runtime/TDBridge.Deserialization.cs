e6d6889053ad256204a54f3a5f85d3f14d96714b
SeanNi1221 <sean.ni.21@outlook.com>
2021-09-01 12:25:52 +0800

    Add TDBridge.Deserialization

---
+++ Runtime/TDBridge.Deserialization.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Sean21
+{
+public partial class TDBridge
+{
+    public static object FromTD<T>(Result result, int row = 0) {
+        T obj;
+        //Ignore timestamp primary key.
+        for (int i= 0;;) {}
+        foreach ( var field in typeof(T).GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic) ) {
+        }
+    }
+    public static object FromTD(Result result, Type type, int row = 0) {
+        // typeof(type) obj;
+        // var _obj = obj as typof(type); 
+    }
+}
+}
